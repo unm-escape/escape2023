009f9248e2c1454372e6d9e272e68dbfc01afd4a
Eric Lindsey <ericlindsey@users.noreply.github.com>
2023-08-15 22:46:24 -0600

    add day9 files

---
+++ day09_Aug17/kml_functions.py
@@ -0,0 +1,106 @@
+# define functions used to convert the velocity image to a KML
+
+# code from: https://ocefpaf.github.io/python4oceanographers/blog/2014/03/10/gearth/
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+# might need to install the simplekml package. Use Conda however you normally use it to install packages.
+#!conda install -c conda-forge simplekml
+from simplekml import (Kml, OverlayXY, ScreenXY, Units, RotationXY, AltitudeMode, Camera)
+
+
+def downsample_image(image, block_size=(9, 9)):
+    """
+    Downsample a 2D array by replacing each block of size block_size 
+    with its mean value, safely handling NaN values.
+    """
+    # Compute the output shape
+    out_shape = (image.shape[0] // block_size[0], image.shape[1] // block_size[1])
+    downsampled = np.zeros(out_shape)
+
+    for i in range(out_shape[0]):
+        for j in range(out_shape[1]):
+            block = image[i*block_size[0]:(i+1)*block_size[0], j*block_size[1]:(j+1)*block_size[1]]
+            downsampled[i, j] = np.nanmean(block)
+    return downsampled
+
+
+def make_kml(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat,
+             figs, colorbar=None, **kw):
+    """TODO: LatLon bbox, list of figs, optional colorbar figure,
+    and several simplekml kw..."""
+
+    kml = Kml()
+    altitude = kw.pop('altitude', 2e6)
+    roll = kw.pop('roll', 0)
+    tilt = kw.pop('tilt', 0)
+    altitudemode = kw.pop('altitudemode', AltitudeMode.relativetoground)
+    camera = Camera(latitude=np.mean([urcrnrlat, llcrnrlat]),
+                    longitude=np.mean([urcrnrlon, llcrnrlon]),
+                    altitude=altitude, roll=roll, tilt=tilt,
+                    altitudemode=altitudemode)
+
+    kml.document.camera = camera
+    draworder = 0
+    for fig in figs:  # NOTE: Overlays are limited to the same bbox.
+        draworder += 1
+        ground = kml.newgroundoverlay(name='GroundOverlay')
+        ground.draworder = draworder
+        ground.visibility = kw.pop('visibility', 1)
+        ground.name = kw.pop('name', 'overlay')
+        ground.color = kw.pop('color', 'ffffffff') # first two f's set transparency. Originally "9e" (50%)
+        ground.atomauthor = kw.pop('author', 'ocefpaf')
+        ground.latlonbox.rotation = kw.pop('rotation', 0)
+        ground.description = kw.pop('description', 'Matplotlib figure')
+        ground.gxaltitudemode = kw.pop('gxaltitudemode',
+                                       'clampToSeaFloor')
+        ground.icon.href = fig
+        ground.latlonbox.east = llcrnrlon
+        ground.latlonbox.south = llcrnrlat
+        ground.latlonbox.north = urcrnrlat
+        ground.latlonbox.west = urcrnrlon
+
+    if colorbar:  # Options for colorbar are hard-coded (to avoid a big mess).
+        screen = kml.newscreenoverlay(name='Colorbar Overlay')
+        screen.icon.href = colorbar
+        screen.overlayxy = OverlayXY(x=0, y=0,
+                                     xunits=Units.fraction,
+                                     yunits=Units.fraction)
+        screen.screenxy = ScreenXY(x=0.015, y=0.075,
+                                   xunits=Units.fraction,
+                                   yunits=Units.fraction)
+        screen.rotationXY = RotationXY(x=0.5, y=0.5,
+                                       xunits=Units.fraction,
+                                       yunits=Units.fraction)
+        screen.size.x = 0
+        screen.size.y = 0
+        screen.size.xunits = Units.fraction
+        screen.size.yunits = Units.fraction
+        screen.visibility = 1
+
+    kmzfile = kw.pop('kmzfile', 'overlay.kmz')
+    kml.savekmz(kmzfile)
+    
+def gearth_fig(llcrnrlon, llcrnrlat, urcrnrlon, urcrnrlat, pixels=1024):
+    """Return a Matplotlib `fig` and `ax` handles for a Google-Earth Image."""
+    aspect = np.cos(np.mean([llcrnrlat, urcrnrlat]) * np.pi/180.0)
+    xsize = np.ptp([urcrnrlon, llcrnrlon]) * aspect
+    ysize = np.ptp([urcrnrlat, llcrnrlat])
+    aspect = ysize / xsize
+
+    if aspect > 1.0:
+        figsize = (10.0 / aspect, 10.0)
+    else:
+        figsize = (10.0, 10.0 * aspect)
+
+    if False:
+        plt.ioff()  # Make `True` to prevent the KML components from poping-up.
+    fig = plt.figure(figsize=figsize,
+                     frameon=False,
+                     dpi=pixels//10)
+    # KML friendly image.  If using basemap try: `fix_aspect=False`.
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_xlim(llcrnrlon, urcrnrlon)
+    ax.set_ylim(llcrnrlat, urcrnrlat)
+    return fig, ax
